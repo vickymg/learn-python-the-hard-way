206bee9e7e0c13e86ff15e89ad7e87029e6e7a43
Vicky Gray <vickygray26@hotmail.com>
2016-07-03 22:00:08 +0100

    exercise_25

---
+++ exercise_25.py
@@ -0,0 +1,54 @@
+# Even More Practice
+
+# To run in Python:
+#   >>> import exercise_25
+#   >>> sentence = "All good things come to those who wait."
+#   >>> words = exercise_25.break_words(sentence)
+#   >>> words
+#   >>> sorted_words = exercise_25.sort_words(words)
+#   >>> sorted_words
+#   >>> exercise_25.print_first_word(words)
+#   >>> exercise_25.print_last_word(words)
+#   >>> words
+#   >>> exercise_25.print_first_word(sorted_words)
+#   >>> exercise_25.print_last_word(sorted_words)
+#   >>> sorted_words
+#   >>> sorted_words = exercise_25.sort_sentence(sentence)
+#   >>> sorted_words
+#   >>> exercise_25.print_first_and_last(sentence)
+#   >>> exercise_25.print_first_and_last_sorted(sentence)
+
+def break_words(stuff):
+    """This function will break up words for us."""
+    words = stuff.split(' ')
+    return words
+
+def sort_words(words):
+    """Sorts the words."""
+    return sorted(words)
+
+def print_first_word(words):
+    """Prints the first word after popping it off."""
+    word = words.pop(0)
+    print word
+
+def print_last_word(words):
+    """Prints the last word after popping it off."""
+    word = words.pop(-1)
+    print word
+
+def sort_sentence(sentence):
+    """Takes in a full sentence and returns the sorted words."""
+    words = break_words(sentence)
+    return sort_words(sentence)
+
+def print_first_and_last(sentence):
+    """Prints the first and last words of the sentence."""
+    words = break_words(sentence)
+    return sort_words(words)
+
+def print_first_and_last_sorted(sentence):
+    """Sorts the words then prints the first and last one."""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
