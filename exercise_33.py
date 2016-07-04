832c17ac8248cd893bf1b8b9ae0b420ad72052cf
Vicky Gray <vickygray26@hotmail.com>
2016-07-04 23:42:50 +0100

    exercise 33

---
+++ exercise_33.py
@@ -0,0 +1,40 @@
+# While Loops
+
+i = 0
+numbers = []
+
+while i < 6:
+    print "At the top i is %d" % i
+    numbers.append(i)
+
+    i = i + 1
+    print "Numbers now: ", numbers
+    print "At the bottom i is %d" % i
+
+print "The numbers: "
+
+for num in numbers:
+    print num
+
+# Example running the below function:
+
+# $ python
+# >>> import exercise_33
+# >>> exercise_33.add_numbers(10)
+
+def add_numbers(num):
+    i = 0
+    numbers = []
+
+    while i < num:
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        i = i + 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+
+    print "The numbers: "
+
+    for num in numbers:
+        print num
