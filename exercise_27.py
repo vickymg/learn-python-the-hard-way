2538e333a57cfccfe1d30ae69542d64db602c820
Vicky Gray <vickygray26@hotmail.com>
2016-07-04 17:12:10 +0100

    exercise 27

---
+++ exercise_27.py
@@ -0,0 +1,58 @@
+# Memorizing logic
+
+# and
+# or
+# not
+# ! = (not equal)
+# == (equal)
+# >= (greater than or equal)
+# <= (less than or equal)
+# true
+# false
+
+#      NOT              True?
+# -----------------------------------
+#   not false           true
+#   not True            false
+
+#      OR               True?
+# -----------------------------------
+#   true or false       true
+#   true or true        true
+#   false or true       true
+#   false or false      false
+
+#      AND              TRUE?
+# -----------------------------------
+#   true and false      false
+#   true and true       true
+#   false and true      false
+#   false and false     false
+
+#     NOT OR                 TRUE?
+# -----------------------------------
+#   not (true or false)      false
+#   not (true or true)       false
+#   not (false or true)      false
+#   not (false or false)     true
+
+#     NOT AND                TRUE?
+# -----------------------------------
+#   not (true and false)     true
+#   not (true and true)      false
+#   not (false and true)     true
+#   not (false and false)    true
+
+#       !=              True?
+# -------------------------------
+#    1 != 0             true
+#    1 1= 1             false
+#    0 != 1             true
+#    0 != 0             false
+
+#       ==              True?
+# --------------------------------
+#   1 == 0              false
+#   1 == 1              true
+#   0 == 1              false
+#   0 == 0              true
