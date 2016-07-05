0d652cd088e4ab50d18499c731398ccdb5d2b711
Vicky Gray <vickygray26@hotmail.com>
2016-07-05 17:51:34 +0100

    exercise 34

---
+++ exercise_34.py
@@ -0,0 +1,35 @@
+# Accessing Elements of Lists
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# 1. The animal at 1
+#   --> python
+print animals[1]
+
+# 2. The third animal
+#   --> peacock
+print animals[2]
+
+# 3. The first animal
+#   --> bear
+print animals[0]
+
+# 4. The animal at 3
+#   --> kangaroo
+print animals[3]
+
+# 5. The fifth animal
+#   --> whale
+print animals[4]
+
+# 6. The animal at 2
+#   --> peacock
+print animals[2]
+
+# 7. The sixth animal
+#   --> platypus
+print animals[5]
+
+# 8. The animal at 4
+#   --> whale
+print animals[4]
