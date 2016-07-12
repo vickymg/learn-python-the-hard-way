c16a288fe744541ec8e25e4dd8d5c42c7f5b3326
Vicky Gray <vickygray26@hotmail.com>
2016-07-12 16:35:41 +0100

    exercise 41

---
+++ exercise_41.py
@@ -0,0 +1,93 @@
+# Learning to speak Object-Oriented
+
+# class - tell Python to make a new kind of thing
+# object - the most basic kind of thing, or any instance of some thing
+# instance - what you get when you tell Python to create a class
+# def - how you define a function inside a class
+# self - inside the functions in a class, self is a variable for the instance/object being accessed
+# inheritance - the concept that one class can inherit traits from another class
+# composition - the concept that a class can be composed of other classes as parts
+# attribute - a property classes have that are from composition, and usually variables
+# is-a - a phrase to say that something inherits from another
+# has-a - a phrase to say that something is compoased of other things or has a trait
+
+import random
+from urllib import urlopen
+import sys
+
+WORD_URL = "http://learncodethehardway.org/words.txt"
+WORDS = []
+
+PHRASES = {
+    "class %%%(%%%):":
+      "Make a class named %%% that is-a %%%.",
+    "class %%%(object):\n\tdef __init__(self, ***)" :
+      "class %%% has-a __init__ that takes self and *** parameters.",
+    "class %%%(object):\n\tdef ***(self, @@@)":
+      "class %%% has-a function named *** that takes self and @@@ parameters.",
+    "*** = %%%()":
+      "Set *** to an instance of class %%%.",
+    "***.***(@@@)":
+      "From *** get the *** function, and call it with parameters self, @@@.",
+    "***.*** = '***'":
+      "From *** get the *** attribute and set it to '***'."
+}
+
+# do they want to drill phrases first
+if len(sys.argv) == 2 and sys.argv[1] == "english":
+    PHRASE_FIRST = True
+else:
+    PHRASE_FIRST = False
+
+# load up the words from the website
+for word in urlopen(WORD_URL).readlines():
+    WORDS.append(word.strip())
+
+def convert(snippet, phrase):
+    class_names = [w.capitalize() for w in random.sample(WORDS, snippet.count("%%%"))]
+    other_names = random.sample(WORDS, snippet.count("***"))
+    results = []
+    param_names = []
+
+    for i in range(0, snippet.count("@@@")):
+        param_count = random.randint(1,3)
+        param_names.append(', '.join(random.sample(WORDS, param_count)))
+
+    for sentence in snippet, phrase:
+        result = sentence[:]
+
+        # fake class names
+        for word in class_names:
+            result = result.replace("%%%", word, 1)
+
+        # fake other names
+        for word in other_names:
+            result = result.replace("***", word, 1)
+
+        # fake parameter lists
+        for word in param_names:
+            result = result.replace("@@@", word, 1)
+
+        results.append(result)
+
+    return results
+
+
+# keep going until they hit CTRL-D
+try:
+    while True:
+        snippets = PHRASES.keys()
+        random.shuffle(snippets)
+
+        for snippet in snippets:
+            phrase = PHRASES[snippet]
+            question, answer = convert(snippet, phrase)
+            if PHRASE_FIRST:
+                question, answer = answer, question
+
+            print question
+
+            raw_input("> ")
+            print "ANSWER:  %s\n\n" % answer
+except EOFError:
+    print "\nBye"
